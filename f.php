14132b9d8c9604494f8c614cb090598bddd88df0
SUBHASHIS SARKAR <132819455+Subhashis-sSarkar@users.noreply.github.com>
2024-10-26 12:17:16 +0530

    Add files via upload

---
+++ f.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your website address is: <?php echo $_POST["website"]; ?>
+
+</body>
+</html>
---
+++ welcom.php
@@ -0,0 +1,7 @@
+<?php 
+ $a = 'subhashis';
+ var_dump($a);
+
+
+       
+?>
